7c5df2d981fe8a6d31eb2148c70270337dbd0409
Penn <ye4241@Penns-iMac-Home.lan>
2019-11-30 16:50:14 +0800

    fix version mistake in url;

--- Casks/electron-ssr.rb
+++ Casks/electron-ssr.rb
@@ -2,7 +2,7 @@ cask 'electron-ssr' do
     version '0.2.7'
     sha256 'cab012e7032b5feb391419f6c18ea55eb838d220dcb7b1d9fe2dfc4429a3266b'
   
-    url "https://github.com/shadowsocksrr/electron-ssr/releases/download/v{version}/electron-ssr-{version}.dmg"
+    url "https://github.com/shadowsocksrr/electron-ssr/releases/download/v#{version}/electron-ssr-#{version}.dmg"
     name 'Electron SSR'
     homepage 'https://github.com/shadowsocksrr/electron-ssr/releases'
   
