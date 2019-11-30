bdc12323c72296d1d7b12969caca673cdaa2e1fb
ye4241 <ye4241@gmail.com>
2019-11-30 15:49:00 +0800

    Update shadowsocksx-ng-r.rb

--- Casks/shadowsocksx-ng-r.rb
+++ Casks/shadowsocksx-ng-r.rb
@@ -2,7 +2,7 @@ cask 'shadowsocksx-ng-r' do
   version '1.4.4-r8'
   sha256 '9353980f35f78a9d16951f28d036f5647a2f5a5c1a79f6480cc4b2852bc77e57'
 
-  url "https://github.com/qinyuhang/ShadowsocksX-NG-R/releases/download/{version}/ShadowsocksX-NG-R8.dmg"
+  url "https://github.com/qinyuhang/ShadowsocksX-NG-R/releases/download/1.4.4-r8/ShadowsocksX-NG-R8.dmg"
   name 'ShadowsocksX NG R8'
   homepage 'https://github.com/qinyuhang/ShadowsocksX-NG-R/releases'
 
